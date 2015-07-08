7d0207c58d6f1bc795068ed83eb3c6c92fd2429f
Jinyoung Eom <jinyoung.eom@gmail.com>
2015-07-08 13:07:02 +0900

    .

---
+++ java01t/src/net/bitacademy/java72/step07/v01/BoardApp.java
@@ -0,0 +1,111 @@
+package net.bitacademy.java72.step07.v01;
+
+import java.util.List;
+import java.util.Scanner;
+
+public class BoardApp {
+  static Scanner scanner;
+  
+  public static void main(String[] args) {
+    scanner = new Scanner(System.in);
+    String command = null;
+    
+    DBConnectionPool dbPool = new DBConnectionPool(
+        "com.mysql.jdbc.Driver",
+        "jdbc:mysql://localhost:3306/java72db",    
+        "java72", 
+        "java72");
+    
+    BoardDao boardDao = new BoardDao(dbPool);
+    
+    do {
+      System.out.print("명령> ");
+      command = scanner.nextLine().toLowerCase();
+      
+      if (command.equals("list")) {
+        List<Board> boards = boardDao.list();
+        for (Board board : boards) {
+          System.out.printf("%d, %s, %s, %d\n", 
+              board.getNo(),
+              board.getTitle(),
+              board.getCreateDate(),
+              board.getViewCount());
+        }
+        
+      } else if (command.equals("insert")) {
+        Board board = new Board();
+        
+        System.out.print("제목? ");
+        board.setTitle(scanner.nextLine());
+        
+        System.out.print("내용? ");
+        board.setContent(scanner.nextLine());
+        
+        System.out.print("암호? ");
+        board.setPassword(scanner.nextLine());
+        
+        int count = boardDao.insert(board);
+        if (count == 0) {
+          System.out.println("입력 실패!");
+        } else {
+          System.out.println("입력 성공!");
+        }
+      
+      } else if (command.equals("update")) {
+        System.out.print("변경할 게시물 번호? ");
+        int no = Integer.parseInt(scanner.nextLine());
+        
+        // 해당 게시물의 데이터를 가져온다.
+        Board board = boardDao.get(no);
+        
+        // 사용자로부터 변경 내용을 입력 받는다.
+        System.out.printf("제목(%s)? ", board.getTitle());
+        String newTitle = scanner.nextLine();
+        if (newTitle.length() > 0) {
+          board.setTitle(newTitle);
+        }
+        
+        System.out.printf("내용(%s)? ", board.getContent());
+        String newContent = scanner.nextLine();
+        if (newContent.length() > 0) {
+          board.setContent(newContent);
+        }
+        
+        // 입력 받은 내용으로 게시물을 변경한다.
+        int count = boardDao.update(board);
+        if (count == 0) {
+          System.out.println("변경 실패!");
+        } else {
+          System.out.println("변경 성공!");
+        }
+        
+      } else if (command.equals("delete")) {
+        System.out.print("삭제할 게시물 번호? ");
+        String no = scanner.nextLine();
+        
+        boardDao.delete(no);
+        
+      } else if (command.equals("quit")) {
+        System.out.println("안녕히 가세요!");
+      
+      } else {
+        System.out.println("해당 명령을 지원하지 않습니다!");
+      }
+      
+      
+    } while (!command.toLowerCase().equals("quit"));
+    
+    dbPool.closeAll();
+    
+    scanner.close();
+  }
+}
+
+
+
+
+
+
+
+
+
---
+++ java01t/src/net/bitacademy/java72/step07/v01/BoardDao.java
@@ -0,0 +1,159 @@
+package net.bitacademy.java72.step07.v01;
+
+import java.io.InputStream;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.Statement;
+import java.util.List;
+
+import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+
+public class BoardDao {
+  DBConnectionPool dbPool;
+  
+  public BoardDao(DBConnectionPool dbPool) {
+    this.dbPool = dbPool;
+  }
+  
+  public List<Board> list() {
+    SqlSession sqlSession = null;
+    try {
+      // mybatis 설정 파일이 있는 경로를 지정한다.
+      String resource = "net/bitacademy/java72/step07/v01/mybatis-config.xml";
+      
+      // 위에서 가리키는 경로에서 mybatis 설정 파일을 읽을 도구를 준비한다.
+      InputStream inputStream = Resources.getResourceAsStream(resource);
+      
+      // mybatis 설정 파일에 맞추어 SqlSessionFactory를 준비한다.
+      SqlSessionFactory sqlSessionFactory = new SqlSessionFactoryBuilder().build(inputStream);
+      
+      // SqlSessionFactory 클래스로부터 실제 SQL 작업을 수행할 SqlSession을 얻는다.
+      sqlSession = sqlSessionFactory.openSession();
+
+      // SqlSession을 사용하여 SQL 맵퍼 파일에 있는 SQL문을 실행한다.
+      return sqlSession.selectList(
+          "net.bitacademy.java72.step07.v01.BoardDao.list");
+      
+    } catch (Exception e) {
+      e.printStackTrace();
+      return null;
+      
+    } finally {
+      sqlSession.close();
+    }
+  }
+  
+  public void delete(String no) {
+    Connection con = null;
+    Statement stmt = null;
+    
+    try {
+      con = dbPool.getConnection();
+      stmt = con.createStatement();
+      int count = stmt.executeUpdate(
+          "DELETE FROM board10 WHERE bno=" + no);
+      System.out.printf("삭제 완료!: %d\n", count);
+      
+    } catch (Exception e) {
+      e.printStackTrace();
+
+    } finally {
+      try {stmt.close();} catch (Exception e) {}
+      dbPool.returnConnection(con);
+    }
+  }
+
+  public int update(Board board) {
+    Connection con = null;
+    PreparedStatement stmt = null;
+    
+    try {
+      con = dbPool.getConnection();
+      stmt = con.prepareStatement(
+          "UPDATE board10 SET title=?,content=? WHERE bno=?");
+      stmt.setString(1, board.getTitle());
+      stmt.setString(2, board.getContent());
+      stmt.setInt(3, board.getNo());
+      return stmt.executeUpdate();
+      
+    } catch (Exception e) {
+      e.printStackTrace();
+      return 0;
+    } finally {
+      try {stmt.close();} catch (Exception e) {}
+      dbPool.returnConnection(con);
+    }
+  }
+
+  public int insert(Board board) {
+    Connection con = null;
+    PreparedStatement stmt = null;
+    
+    try {
+      con = dbPool.getConnection();
+      stmt = con.prepareStatement( 
+          "INSERT INTO board10(title,content,pwd,cre_dt)"
+          + " values(?, ?, ?, now())");
+      stmt.setString(1, board.getTitle());
+      stmt.setString(2, board.getContent());
+      stmt.setString(3, board.getPassword());
+      return stmt.executeUpdate();
+      
+    } catch (Exception e) {
+      e.printStackTrace();
+      return 0;
+    } finally {
+      try {stmt.close();} catch (Exception e) {}
+      dbPool.returnConnection(con);
+    }
+  }
+
+  public Board get(int no) {
+    Connection con = null;
+    Statement stmt = null;
+    ResultSet rs = null;
+    
+    try {
+      con = dbPool.getConnection();
+      stmt = con.createStatement();
+      rs = stmt.executeQuery(
+          "SELECT bno,title,content"
+          + " FROM board10"
+          + " WHERE bno=" + no);
+      
+      Board board = null;
+      if (rs.next()) {
+        board = new Board();
+        board.setNo(rs.getInt("bno"));
+        board.setTitle(rs.getString("title"));
+        board.setContent(rs.getString("content"));
+      }
+      return board;
+
+    } catch (Exception e) {
+      e.printStackTrace();
+      return null;
+      
+    } finally {
+      try {rs.close();} catch (Exception e) {}
+      try {stmt.close();} catch (Exception e) {}
+      dbPool.returnConnection(con);
+    }
+  }
+  
+}
+
+
+
+
+
+
+
+
+
+
+
