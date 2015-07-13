ad26bc17865d1bc3b56dfe5a960f8e73a1d793e5
jinyoug <bit@bit-PC>
2015-07-13 15:04:47 +0900

    .

--- java01t/src/net/bitacademy/java72/step07/v09/BoardDao.java
+++ java01t/src/net/bitacademy/java72/step07/v09/BoardDao.java
@@ -22,7 +22,7 @@ public class BoardDao {
 
       // SqlSession을 사용하여 SQL 맵퍼 파일에 있는 SQL문을 실행한다.
       return sqlSession.selectList(
-          "net.bitacademy.java72.step07.v02.BoardDao.list");
+          "net.bitacademy.java72.step07.v09.BoardDao.list");
       
     } catch (Exception e) {
       e.printStackTrace();
@@ -38,7 +38,7 @@ public class BoardDao {
     try {
       sqlSession = sqlSessionFactory.openSession();
       int count = sqlSession.delete(
-          "net.bitacademy.java72.step07.v02.BoardDao.delete",
+          "net.bitacademy.java72.step07.v09.BoardDao.delete",
           no);
       /* DML(Data Manipulation Language; insert, update, delete)
        * 명령의 결과를 실제 테이블에 반영하라고 지시한다.
@@ -60,7 +60,7 @@ public class BoardDao {
     try {
       sqlSession = sqlSessionFactory.openSession();
       int count = sqlSession.update(
-          "net.bitacademy.java72.step07.v02.BoardDao.update",
+          "net.bitacademy.java72.step07.v09.BoardDao.update",
           board);
       sqlSession.commit();
       return count;
@@ -79,7 +79,7 @@ public class BoardDao {
     try {
       sqlSession = sqlSessionFactory.openSession();
       int count = sqlSession.insert(
-          "net.bitacademy.java72.step07.v02.BoardDao.insert",
+          "net.bitacademy.java72.step07.v09.BoardDao.insert",
           board);
       sqlSession.commit();
       return count;
@@ -98,7 +98,7 @@ public class BoardDao {
     try {
       sqlSession = sqlSessionFactory.openSession();
       return sqlSession.selectOne(
-          "net.bitacademy.java72.step07.v02.BoardDao.get",
+          "net.bitacademy.java72.step07.v09.BoardDao.get",
           no);
       
     } catch (Exception e) {
