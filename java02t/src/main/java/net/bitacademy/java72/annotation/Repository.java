65f055cfde0cb5af01fcee11f5df3c876ae54fbe
jinyoug <bit@bit-PC>
2015-07-15 11:19:20 +0900

    .

--- java02t/src/main/java/net/bitacademy/java72/annotation/Repository.java
+++
@@ -1,9 +0,0 @@
-package net.bitacademy.java72.annotation;
-
-import java.lang.annotation.Retention;
-import java.lang.annotation.RetentionPolicy;
-
-@Retention(RetentionPolicy.RUNTIME)
-public @interface Repository {
-  String value() default "";
-}
