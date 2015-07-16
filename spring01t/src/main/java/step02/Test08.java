7301f786fcda35461a0d484922d191e39f58df0f
jinyoug <bit@bit-PC>
2015-07-16 10:28:56 +0900

    .

--- spring01t/src/main/java/step02/Test08.java
+++ spring01t/src/main/java/step02/Test08.java
@@ -8,15 +8,12 @@ public class Test08 {
   public static void main(String[] args) {
     ApplicationContext iocContainer = 
         new ClassPathXmlApplicationContext(
-            "step01/application-context07.xml");
+            "step02/application-context08.xml");
     
     System.out.println(iocContainer.getBean("c1"));
     System.out.println(iocContainer.getBean("c2"));
     System.out.println(iocContainer.getBean("c3"));
-    System.out.println(iocContainer.getBean("c4"));
-    System.out.println(iocContainer.getBean("c5"));
-    System.out.println(iocContainer.getBean("c6"));
-    System.out.println(iocContainer.getBean("c7"));
+    
   }
 
 }
