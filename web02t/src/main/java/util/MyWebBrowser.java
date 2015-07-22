27d3f2dc7c0cb639b6658a68bfa68bfa8b74ce0c
jinyoug <bit@bit-PC>
2015-07-22 10:26:39 +0900

    .

---
+++ web02t/src/main/java/util/MyWebBrowser.java
@@ -0,0 +1,51 @@
+package util;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.PrintStream;
+import java.net.Socket;
+
+public class MyWebBrowser {
+
+  public static void main(String[] args) throws Exception {
+    String host = "www.naver.com";
+    
+    Socket socket = new Socket(host, 80);
+    PrintStream out = 
+        new PrintStream(socket.getOutputStream());
+    BufferedReader in = 
+        new BufferedReader(
+            new InputStreamReader(
+                socket.getInputStream()));
+    
+    out.println("GET / HTTP/1.1");
+    out.println("Host: www.naver.com");
+    out.println();
+    
+    String line = null;
+    while ((line = in.readLine()).equals("")) {
+      System.out.println(line);
+    }
+    
+    char[] buf = new char[1024];
+    int len = 0;
+    while ((len = in.read(buf)) > 0) {
+      System.out.println(String.valueOf(buf, 0, len));
+    }
+    
+    in.close();
+    out.close();
+    socket.close();
+    
+
+  }
+
+}
+
+
+
+
+
+
+
+
