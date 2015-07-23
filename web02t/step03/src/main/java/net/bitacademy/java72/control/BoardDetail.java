098564f85c833cd9924202679f87b5f7712bbde2
jinyoug <bit@bit-PC>
2015-07-23 16:22:03 +0900

    .

---
+++ web02t/step03/src/main/java/net/bitacademy/java72/control/BoardDetail.java
@@ -0,0 +1,101 @@
+package net.bitacademy.java72.control;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.GenericServlet;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+
+import net.bitacademy.java72.context.MyApplicationContext;
+import net.bitacademy.java72.dao.BoardDao;
+import net.bitacademy.java72.domain.Board;
+
+public class BoardDetail extends GenericServlet {
+  private static final long serialVersionUID = 1L;
+
+  @Override
+  public void service(
+      ServletRequest request, 
+      ServletResponse response) throws ServletException, IOException {
+    MyApplicationContext context = 
+        MyApplicationContext.getInstance();
+    
+    BoardDao boardDao = (BoardDao)context.getBean("boardDao");
+      
+    int no = Integer.parseInt(request.getParameter("no"));
+    Board board = boardDao.get(no);
+
+    response.setContentType("text/html;charset=UTF-8");
+    PrintWriter out = response.getWriter();
+    
+    out.println("<!DOCTYPE html>");
+    out.println("<html>");
+    out.println("<head>");
+    out.println("  <meta charset='UTF-8'>");
+    out.println("  <title>게시물 상세보기</title>");
+    out.println("  <style>");
+    out.println("    table {");
+    out.println("      border-collapse: separate;");
+    out.println("    }");
+    out.println("    th {");
+    out.println("      background-color: lightgray;");
+    out.println("      padding: 5px;");
+    out.println("    }");
+    out.println("  </style>");
+    out.println("</head>");
+    out.println("<body>");
+    out.println("<h1>게시물 상세보기</h1>");
+    out.println("<form action='update.do' method='get'>");
+    out.println("<table>");
+    if (board != null) {
+      out.printf("<tr><th>번호</th>"
+          + "<td>"
+          + "<input type='text' readonly"
+          + " name='no' value='%d'>"
+          + "</td></tr>\n" 
+          , board.getNo());
+      out.printf("<tr><th>제목</th>"
+          + "<td>"
+          + "<input type='text' name='title' value='%s'>"
+          + "</td></tr>\n"
+          , board.getTitle());
+      out.printf("<tr><th>내용</th>"
+          + "<td><textarea name='content'"
+          + " rows='5' cols='50'>%s</textarea>"
+          + "</td></tr>\n"
+          , board.getContent());
+      out.printf("<tr><th>조회수</th><td>%d</td></tr>\n"
+          , board.getViewCount());
+      out.printf("<tr><th>등록일</th><td>%s</td></tr>\n"
+          , board.getCreateDate());
+    } else {
+      out.println("<tr><td>"
+          + "해당 번호의 게시물을 찾을 수 없습니다."
+          + "</td></tr>");
+    }
+    out.println("<tr>");
+    out.println("  <th></th>");
+    out.printf("  <td>"
+        + "<input type='submit' value='변경'>"
+        + "<a href='delete.do?no=%d'>삭제</a>"
+        + "</td>"
+        , board.getNo());
+    out.println("</tr>");
+    out.println("</table>");
+    out.println("</form>");
+    out.println("<p><a href='list.do'>목록</a><p>");
+    out.println("</body>");
+    out.println("</html>");
+  }
+
+}
+
+
+
+
+
+
+
+
