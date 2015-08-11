323a640637000b696860f776d218e62fee120438
jinyoug <bit@bit-PC>
2015-08-11 09:54:14 +0900

    .

--- spring02t/step12/src/main/java/study/controller/TestController.java
+++ spring02t/step12/src/main/java/study/controller/TestController.java
@@ -4,16 +4,16 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import study.service.TxTestService;
+import study.service.TestService;
 import study.vo.Test01;
 import study.vo.Test02;
 
 @Controller
-public class TxTestController {
-  @Autowired TxTestService txTestService;
+public class TestController {
+  @Autowired TestService testService;
   
-  @RequestMapping("/study/txtest.do")
-  public String txTest(String title1, String title2) {
+  @RequestMapping("/study/insert.do")
+  public String insert(String title1, String title2) {
     
     try {
       Test01 test01 = new Test01();
@@ -22,7 +22,7 @@ public class TxTestController {
       Test02 test02 = new Test02();
       test02.setTitle(title2);
       
-      txTestService.test(test01, test02);
+      testService.addTest(test01, test02);
       
     } catch (Exception e) {
       e.printStackTrace();
