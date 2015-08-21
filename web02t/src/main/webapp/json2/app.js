8ac5bfbf6d9e30235dd0b7245129f79bae00ed22
jinyoug <bit@bit-PC>
2015-08-21 16:08:05 +0900

    .

---
+++ web02t/src/main/webapp/json2/app.js
@@ -0,0 +1,10 @@
+$('#main > header').load('header.html');
+$('#main > footer').load('footer.html');
+
+$('#boardMenu').click(function(event) {
+  alert('게시판 관리');
+});
+
+$('#memberMenu').click(function(event) {
+  alert('회원 관리');
+});
---
+++ web02t/src/main/webapp/json2/header.js
@@ -0,0 +1,45 @@
+//사용자 정보 감추기
+$('#userInfo').css('display', 'none');
+
+//로그인 정보 가져오기
+$.getJSON(contextRoot + '/json/auth/loginInfo.do', function(result) {
+  if (result.state == 'yes') {  
+    $('#userName').text(result.data.name);
+    $('#userEmail').text(result.data.email);
+    $('#loginBtn').css('display', 'none');
+    $('#userInfo').css('display', '');
+  }
+});
+
+$('#loginBtn').click(function(event) {
+  event.preventDefault();
+  $('#content').load('sub/auth.html');
+});
+  
+$('#logoutBtn').click(function(event) {
+  event.preventDefault();
+  $.getJSON(contextRoot + '/json/auth/logout.do', function(result) {
+    location.href = contextRoot + '/json/auth/index.html';
+  });
+});
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+
+
+
+
